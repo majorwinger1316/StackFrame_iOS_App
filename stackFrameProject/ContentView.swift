3840da3aa797a49720e0cf00e8368d0160bcd362
Akshat Dutt Kaushik <akshatdutt1316@gmail.com>
2025-02-05 00:18:26 +0530

    Initial Commit

---
+++ stackFrameProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stackFrameProject
+//
+//  Created by admin@33 on 05/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stackFrameProject/stackFrameProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  stackFrameProjectApp.swift
+//  stackFrameProject
+//
+//  Created by admin@33 on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct stackFrameProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
